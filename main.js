1d88c615f47b654f71f7e39e328c19d8ebfddb01
Quirianyailet Perez <qperez1@usf.edu>
2024-10-25 12:53:22 -0400

    Add event listener for product selection and quantity input

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // Task 1: Setup HTML Structure for Order Form (This is completed in html file)
+
+// Task 2: Add Event Listener for Product Selection
+const quantityElement = document.getElementById("quantity");
+const productSelector = document.getElementById("product-selector");
+
+productSelector.addEventListener("change", (event) => {
+    const selectedPrice = event.target.value;
+    quantityElement.textContent = `$${selectedPrice}`;
+});
